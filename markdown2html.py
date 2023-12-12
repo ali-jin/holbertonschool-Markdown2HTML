e702e30a12e3d6e4975c51c7e9a586f9048f514f
root <root@pc>
2023-12-12 11:14:30 +0100

    edit comment module

--- markdown2html.py
+++ markdown2html.py
@@ -6,9 +6,16 @@ task 0
 
 from os.path import isfile
 import sys
+import markdown
 
 
 def verify_file_exist(argv):
+    """Verify if file exist
+
+    Args:
+        argv[1] (str): file name
+        argv[2] (str): output file name
+    """
     if len(argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
@@ -19,19 +26,29 @@ def verify_file_exist(argv):
         exit(1)
 
 
-def markdown_to_html(line):
-    for i in range(6, 0, -1):
-        if line.startswith('#' * i):
-            return f'<h{i}>{line}'
+def markdown_to_html(argv):
+    """Convert markdown in file into html
+
+    Args:
+        argv[1] (str): file name
+        argv[2] (str): output file name
+    """
+    with open(argv[1], 'r') as file:
+        markdown_string = file.read()
+
+    html_string = markdown.markdown(markdown_string)
+
+    with open(argv[2], 'w') as new_file:
+        new_file.write(html_string)
 
 
 def main():
-    """
-    Convert the markdown in file into HTML in a new file
+    """Convert the markdown in file into HTML in a new file
 
     Verify if file exist and if there are enough arguments
     """
     verify_file_exist(sys.argv)
+    markdown_to_html(sys.argv)
     exit(0)
 
 
