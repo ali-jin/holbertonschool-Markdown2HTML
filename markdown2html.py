95b00838d29072bd1c8796a62d38fa87b461a7b9
root <root@pc>
2023-12-12 14:20:44 +0100

    task 2

--- markdown2html.py
+++ markdown2html.py
@@ -8,18 +8,42 @@ import sys
 
 
 def verify_file_exist(argv):
-
     if len(argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
         exit(1)
 
 
-def markdown_to_html(line):
-    for i in range(6, 0, -1):
-        if line.startswith('#' * i):
-            return f'<h{i}>{line[i+1:].strip()}</h{i}>\n'
-    return line
+def markdown_heading_to_html(lines):
+    lines_list = []
+    for line in lines:
+        for i in range(6, 0, -1):
+            if line.startswith('#' * i):
+                line = f'<h{i}>{line[i+1:].strip()}</h{i}>\n'
+                break
+        lines_list.append(line)
+    return lines_list
+
+
+def markdown_ul_to_html(lines):
+    lines_list = []
+    is_present = False
+
+    for line in lines:
+        if line.startswith('- '):
+            converted_line = f'<li>{line[2:].strip()}</li>\n'
+            if is_present is False:
+                lines_list.append('<ul>\n')
+                is_present = True
+            lines_list.append(converted_line)
+        else:
+            if is_present is True:
+                lines_list.append('</ul>\n')
+                is_present = False
+            lines_list.append(line)
+    if is_present is True:
+        lines_list.append('</ul>\n')
+    return lines_list
 
 
 def main():
@@ -36,9 +60,10 @@ def main():
         exit(1)
 
     with open(markdown_file, 'r') as file, open(sys.argv[2], 'w') as new_file:
-        for line in file:
-            html_string = markdown_to_html(line)
-            new_file.write(html_string)
+        lines = file.readlines()
+        html_lines = markdown_heading_to_html(lines)
+        html_lines = markdown_ul_to_html(html_lines)
+        new_file.writelines(html_lines)
 
     exit(0)
 
