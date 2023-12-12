925ae316d219a1c35c63dd8405bb35f778f0de8b
root <root@pc>
2023-12-12 12:06:12 +0100

    task 1 heading

--- markdown2html.py
+++ markdown2html.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 """
 Markdown to HTML Converter
-task 0
 """
 
 from os.path import isfile
 import sys
-import markdown
 
 
 def verify_file_exist(argv):
-    """Verify if file exist
 
-    Args:
-        argv[1] (str): file name
-        argv[2] (str): output file name
-    """
     if len(argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
@@ -26,29 +19,24 @@ def verify_file_exist(argv):
         exit(1)
 
 
-def markdown_to_html(argv):
-    """Convert markdown in file into html
-
-    Args:
-        argv[1] (str): file name
-        argv[2] (str): output file name
-    """
-    with open(argv[1], 'r') as file:
-        markdown_string = file.read()
-
-    html_string = markdown.markdown(markdown_string)
-
-    with open(argv[2], 'w') as new_file:
-        new_file.write(html_string)
+def markdown_to_html(line):
+    for i in range(6, 0, -1):
+        if line.startswith('#' * i):
+            return f'<h{i}>{line[i+1:].strip()}<h{i}>\n'
 
 
 def main():
     """Convert the markdown in file into HTML in a new file
 
-    Verify if file exist and if there are enough arguments
+    Verify if file exist and if there are enough
     """
     verify_file_exist(sys.argv)
-    markdown_to_html(sys.argv)
+
+    with open(sys.argv[1], 'r') as file, open(sys.argv[2], 'w') as new_file:
+        for line in file:
+            html_string = markdown_to_html(line)
+            new_file.write(html_string)
+
     exit(0)
 
 
